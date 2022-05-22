27bdbe06c23142d7376c5fad09928bf60c93551e
phuocleoceo <ht10082001@gmail.com>
2022-05-22 15:15:56 +0700

    API Folder

---
+++ api/apiService.js
@@ -0,0 +1,30 @@
+import queryString from 'query-string';
+import axios from 'axios';
+
+const BASE_URL = "";
+
+const callAPI = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'content-type': 'application/json'
+    },
+    paramsSerializer: params => queryString.stringify(params)
+});
+
+callAPI.interceptors.request.use((config) =>
+{
+    return config;
+}, (error) =>
+{
+    return Promise.reject(error);
+});
+
+callAPI.interceptors.response.use((response) =>
+{
+    return response;
+}, async (error) =>
+{
+    return Promise.reject(error);
+});
+
+export default callAPI;
