499ba2b2152b25edbee95d8f1f1a3ae07cb87026
phuocleoceo <ht10082001@gmail.com>
2022-05-24 19:35:53 +0700

    Update Tab Navigation

--- screens/Profile/ProfileScreen.js
+++ screens/Profile/ProfileScreen.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 
-export default function PlayerScreen() {
+export default function ProfileScreen()
+{
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text>Player</Text>
+        <Text style={{ marginTop: 100, alignSelf: "center" }}>Profile</Text>
       </View>
     </View>
   );
