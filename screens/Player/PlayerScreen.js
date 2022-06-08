295f7b803c160b34cc42d36c5bbb91ea9cdf1bc1
phuocleoceo <ht10082001@gmail.com>
2022-06-08 22:07:36 +0700

    ExpoAV(bugs)

--- screens/Player/PlayerScreen.js
+++ screens/Player/PlayerScreen.js
@@ -2,176 +2,265 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Slider from '@react-native-community/slider';
 import React, { useState, useEffect } from "react";
+import { Audio } from 'expo-av'
 
 const songs = [
-  {
-    avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/03/12/e/2/9/e/1615554946033.jpg",
-    bgImage: "https://avatar-ex-swe.nixcdn.com/singer/avatar/2021/07/13/0/6/d/2/1626145766324_600.jpg",
-    coverImage: "https://avatar-ex-swe.nixcdn.com/playlist/2021/05/04/3/b/6/d/1620100988545_500.jpg",
-    creator: "Phúc Chinh",
-    lyric: "https://lrc-nct.nixcdn.com/2021/03/22/2/8/d/4/1616360845396.lrc",
-    music: "https://aredir.nixcdn.com/NhacCuaTui1012/TheLuong-PhucChinh-6971140.mp3?st=Gjvx3xDEIBRMoGsc2wLqkA&e=1631635351",
-    title: "Thê Lương",
-    url: "https://www.nhaccuatui.com/bai-hat/the-luong-phuc-chinh.nmxw6tXZyBQy.html"
-  },
-  {
-    avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/09/09/f/c/f/d/1631181753902.jpg",
-    bgImage: "https://avatar-ex-swe.nixcdn.com/singer/avatar/2019/10/29/a/a/d/4/1572318457703_600.jpg",
-    coverImage: "https://avatar-ex-swe.nixcdn.com/playlist/2021/05/04/3/b/6/d/1620100988545_500.jpg",
-    creator: "Masew, Masiu, B Ray, TAP",
-    lyric: "https://lrc-nct.nixcdn.com/null",
-    music: "https://aredir.nixcdn.com/NhacCuaTui1021/CuoiThoi-MasewMasiuBRayTAPVietNam-7085648.mp3?st=Fdf-94PGaMjuqak7C3FJzw&e=1631635351",
-    title: "Cưới Thôi",
-    url: "https://www.nhaccuatui.com/bai-hat/cuoi-thoi-masew-ft-masiu-ft-b-ray-ft-tap.SQTZSysfmPRJ.html"
-  },
-  {
-    avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/08/15/a/7/9/2/1629021746388.jpg",
-    bgImage: "https://avatar-ex-swe.nixcdn.com/singer/avatar/2020/08/13/a/9/8/e/1597294555540_600.jpg",
-    coverImage: "https://avatar-ex-swe.nixcdn.com/playlist/2021/05/04/3/b/6/d/1620100988545_500.jpg",
-    creator: "Tăng Duy Tân",
-    lyric: "https://lrc-nct.nixcdn.com/2021/08/15/d/8/0/6/1629022926567.lrc",
-    music: "https://aredir.nixcdn.com/Warner_Audio72/DaVu-TangDuyTan-7068051.mp3?st=Kp17_rGyQ2uLyHv7v6chDg&e=1631635351",
-    title: "Dạ Vũ",
-    url: "https://www.nhaccuatui.com/bai-hat/da-vu-tang-duy-tan.8Q8yLCcES92H.html"
-  },
+    {
+        avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/03/12/e/2/9/e/1615554946033.jpg",
+        creator: "Phúc Chinh",
+        music: "https://aredir.nixcdn.com/NhacCuaTui1012/TheLuong-PhucChinh-6971140.mp3?st=Gjvx3xDEIBRMoGsc2wLqkA&e=1631635351",
+        title: "Thê Lương",
+    },
+    {
+        avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/09/09/f/c/f/d/1631181753902.jpg",
+        creator: "Masew, Masiu, B Ray, TAP",
+        music: "https://aredir.nixcdn.com/NhacCuaTui1021/CuoiThoi-MasewMasiuBRayTAPVietNam-7085648.mp3?st=Fdf-94PGaMjuqak7C3FJzw&e=1631635351",
+        title: "Cưới Thôi"
+    },
+    {
+        avatar: "https://avatar-ex-swe.nixcdn.com/song/2021/08/15/a/7/9/2/1629021746388.jpg",
+        creator: "Tăng Duy Tân",
+        music: "https://aredir.nixcdn.com/Warner_Audio72/DaVu-TangDuyTan-7068051.mp3?st=Kp17_rGyQ2uLyHv7v6chDg&e=1631635351",
+        title: "Dạ Vũ",
+    },
 ]
 
 export default function PlayerScreen()
 {
-  const [songIndex, setSongIndex] = useState(0);
+    const [player, setPlayer] = useState({
+        isPlaying: false,
+        playbackInstance: null,
+        currentIndex: 0,
+        volume: 1.0,
+        isBuffering: true
+    });
 
-  const handlePreviousPlay = () =>
-  {
-    if (songIndex === 0)
-      setSongIndex(songs.length - 1);
-    else
-      setSongIndex(songIndex => songIndex - 1);
-  };
+    useEffect(() =>
+    {
+        setupPlayer();
+    }, []);
 
-  const handleNextPlay = () =>
-  {
-    if (songIndex === (songs.length - 1))
-      setSongIndex(0);
-    else
-      setSongIndex(songIndex => songIndex + 1);
-  };
+    const loadAudio = async () =>
+    {
+        const { currentIndex, isPlaying, volume } = player;
+        try
+        {
+            const playbackInstance = new Audio.Sound();
+            const source = {
+                uri: songs[currentIndex].music
+            };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.artworkWrapper}>
-        <Image source={{ uri: songs[songIndex].avatar }} style={styles.artworkImage} />
-      </View>
+            const status = {
+                shouldPlay: isPlaying,
+                volume: volume
+            };
 
-      <View style={styles.inforBar}>
-        <Text style={styles.titleInfor}>{songs[songIndex].title}</Text>
-        <Text style={styles.creatorInfor}>{songs[songIndex].creator}</Text>
-      </View>
+            playbackInstance.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+            await playbackInstance.loadAsync(source, status, false);
 
-      <View style={styles.progressSliderBar}>
-        <Slider
-          style={styles.progressSlider}
-          value={10}
-          minimumValue={0}
-          maximumValue={100}
-          thumbTintColor="#2ea9ed"
-          minimumTrackTintColor="#2ea9ed"
-          maximumTrackTintColor="black"
-          onSlidingComplete={() => { }}
-        />
-      </View>
+            setPlayer(player => ({
+                ...player,
+                playbackInstance
+            }));
+        }
+        catch (e)
+        {
+            console.log(e)
+        }
+    };
 
-      <View style={styles.progressLabel}>
-        <Text style={styles.progressLabelText}>0:00</Text>
-        <Text style={styles.progressLabelText}>5:61</Text>
-      </View>
+    const setupPlayer = async () =>
+    {
+        try
+        {
+            await Audio.setAudioModeAsync({
+                allowsRecordingIOS: false,
+                playsInSilentModeIOS: true,
+                shouldDuckAndroid: true,
+                staysActiveInBackground: true,
+                playThroughEarpieceAndroid: true
+            });
+            loadAudio();
+        }
+        catch (e)
+        {
+            console.log(e);
+        }
+    };
 
-      <View style={styles.musicController}>
-        <TouchableOpacity onPress={handlePreviousPlay} activeOpacity={0.5}>
-          <Ionicons name="play-skip-back-outline" size={45}
-            color="black" style={styles.playBackForward} />
-        </TouchableOpacity>
+    const onPlaybackStatusUpdate = (status) =>
+    {
+        setPlayer(player => ({
+            ...player,
+            isBuffering: status.isBuffering
+        }));
+    }
 
-        <TouchableOpacity onPress={() => { }} activeOpacity={0.5}>
-          <Ionicons name="ios-pause-circle" size={80} color="black" />
-        </TouchableOpacity>
+    const handlePlayPause = async () =>
+    {
+        const { isPlaying, playbackInstance } = player;
+        isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+        console.log("Pause/Play");
+        setPlayer(player => ({
+            ...player,
+            isPlaying: !isPlaying
+        }));
+    };
 
-        <TouchableOpacity onPress={handleNextPlay} activeOpacity={0.5}>
-          <Ionicons name="play-skip-forward-outline" size={45}
-            color="black" style={styles.playBackForward} />
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    const handlePreviousPlay = async () =>
+    {
+        let { playbackInstance, currentIndex } = player;
+        if (playbackInstance)
+        {
+            console.log("Previous");
+            await playbackInstance.unloadAsync()
+            currentIndex < songs.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+            setPlayer(player => ({
+                ...player,
+                currentIndex
+            }));
+            loadAudio();
+        }
+    };
+
+    const handleNextPlay = async () =>
+    {
+        let { playbackInstance, currentIndex } = player;
+        if (playbackInstance)
+        {
+            console.log("Next");
+            await playbackInstance.unloadAsync()
+            currentIndex < songs.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+            setPlayer(player => ({
+                ...player,
+                currentIndex
+            }));
+            loadAudio();
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.artworkWrapper}>
+                <Image source={{ uri: songs[player.currentIndex].avatar }} style={styles.artworkImage} />
+            </View>
+
+            <View style={styles.inforBar}>
+                <Text style={styles.titleInfor}>{songs[player.currentIndex].title}</Text>
+                <Text style={styles.creatorInfor}>{songs[player.currentIndex].creator}</Text>
+            </View>
+
+            <View style={styles.progressSliderBar}>
+                <Slider
+                    style={styles.progressSlider}
+                    value={10}
+                    minimumValue={0}
+                    maximumValue={100}
+                    thumbTintColor="#2ea9ed"
+                    minimumTrackTintColor="#2ea9ed"
+                    maximumTrackTintColor="black"
+                    onSlidingComplete={() => { }}
+                />
+            </View>
+
+            <View style={styles.progressLabel}>
+                <Text style={styles.progressLabelText}>0:00</Text>
+                <Text style={styles.progressLabelText}>5:61</Text>
+            </View>
+
+            <View style={styles.musicController}>
+                <TouchableOpacity onPress={handlePreviousPlay} activeOpacity={0.5}>
+                    <Ionicons name="play-skip-back-outline" size={45}
+                        color="black" style={styles.playBackForward} />
+                </TouchableOpacity>
+
+                {/* <TouchableOpacity onPress={() => togglePlayback(playbackState)} activeOpacity={0.5}>
+          <Ionicons name={playbackState === State.Playing ?
+            "ios-pause-circle" : "ios-play-circle"}
+            size={80} color="black" />
+        </TouchableOpacity> */}
+                <TouchableOpacity onPress={handlePlayPause} activeOpacity={0.5}>
+                    <Ionicons name={player.isPlaying ? "ios-pause-circle" : "ios-play-circle"}
+                        size={80} color="black" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleNextPlay} activeOpacity={0.5}>
+                    <Ionicons name="play-skip-forward-outline" size={45}
+                        color="black" style={styles.playBackForward} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  /*-------ARTWORK------*/
-  artworkWrapper: {
-    width: 260,
-    height: 300,
-    marginBottom: 20,
-    marginTop: 25,
-    // iOS
-    shadowColor: "#949996",
-    shadowOffset: {
-      width: 5,
-      height: 5
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
     },
-    shadowOpacity: 0.5,
-    shadowRadius: 3.84,
-    // Android
-    elevation: 5
-  },
-  artworkImage: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 15
-  },
-  /*-------INFOR------*/
-  inforBar: {
-    marginTop: 10
-  },
-  titleInfor: {
-    fontSize: 20,
-    fontWeight: "800",
-    textAlign: "center",
-    color: "black"
-  },
-  creatorInfor: {
-    fontSize: 16,
-    fontWeight: "300",
-    textAlign: "center",
-    color: "black"
-  },
-  /*-------SLIDER------*/
-  progressSlider: {
-    width: 320,
-    height: 40,
-    marginTop: 25,
-    flexDirection: "row"
-  },
-  /*-------TIMER------*/
-  progressLabel: {
-    width: 320,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  progressLabelText: {
-    color: "black"
-  },
-  /*-------CONTROLLER------*/
-  musicController: {
-    flexDirection: "row",
-    width: "60%",
-    justifyContent: "space-between",
-    marginTop: 15
-  },
-  playBackForward: {
-    marginTop: 20
-  }
+    /*-------ARTWORK------*/
+    artworkWrapper: {
+        width: 260,
+        height: 300,
+        marginBottom: 20,
+        marginTop: 25,
+        // iOS
+        shadowColor: "#949996",
+        shadowOffset: {
+            width: 5,
+            height: 5
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+        // Android
+        elevation: 5
+    },
+    artworkImage: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 15
+    },
+    /*-------INFOR------*/
+    inforBar: {
+        marginTop: 10
+    },
+    titleInfor: {
+        fontSize: 20,
+        fontWeight: "800",
+        textAlign: "center",
+        color: "black"
+    },
+    creatorInfor: {
+        fontSize: 16,
+        fontWeight: "300",
+        textAlign: "center",
+        color: "black"
+    },
+    /*-------SLIDER------*/
+    progressSlider: {
+        width: 320,
+        height: 40,
+        marginTop: 25,
+        flexDirection: "row"
+    },
+    /*-------TIMER------*/
+    progressLabel: {
+        width: 320,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    progressLabelText: {
+        color: "black"
+    },
+    /*-------CONTROLLER------*/
+    musicController: {
+        flexDirection: "row",
+        width: "60%",
+        justifyContent: "space-between",
+        marginTop: 15
+    },
+    playBackForward: {
+        marginTop: 20
+    }
 });
