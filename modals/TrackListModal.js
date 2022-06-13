a58cd482ce2d5a4b9be1be0c4091a25a9a12873b
phuocleoceo <ht10082001@gmail.com>
2022-06-14 00:10:57 +0700

    track list style

--- modals/TrackListModal.js
+++ modals/TrackListModal.js
@@ -1,29 +1,40 @@
+import { StyleSheet, Dimensions, TouchableOpacity, View, Image, Text } from "react-native";
 import { RecyclerListView, DataProvider, LayoutProvider } from 'recyclerlistview';
-import { Paragraph, Dialog, Portal } from "react-native-paper";
-import { StyleSheet, Dimensions } from "react-native";
-import * as React from "react";
+import { Dialog, Portal } from "react-native-paper";
+import React from "react";
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
-export default function TrackListModal({ trackList, visible, hideDialog })
+function TrackListModal({ trackList, visible, hideDialog })
 {
     const _dataProvider = new DataProvider((r1, r2) => r1 !== r2).cloneWithRows(trackList);
 
     const _layoutProvider = new LayoutProvider(
         (index) => _dataProvider.getDataForIndex(index),
-        (type, dim) => { dim.width = SCREEN_WIDTH - 50; dim.height = 50; })
+        (type, dim) => { dim.width = SCREEN_WIDTH; dim.height = 80; })
 
     const _rowRenderer = (type, data) =>
     {
-        return <Paragraph>{data.title}</Paragraph>
+        return <TouchableOpacity style={styles.songCard} activeOpacity={0.5}
+            onPress={() => { }}>
+            <View style={styles.imgBox}>
+                <Image style={styles.avatarIMG} source={{ uri: data.avatar }} />
+            </View>
+            <View style={styles.inforBox}>
+                <Text style={styles.songTitle}>{data.title}</Text>
+                <Text style={styles.songCreator}>{data.creator}</Text>
+            </View>
+        </TouchableOpacity>
     };
 
     return (
         <Portal>
             <Dialog visible={visible} onDismiss={hideDialog}
                 style={styles.trackListDialog}>
-                <Dialog.Title>Track List</Dialog.Title>
+                <Dialog.Title>
+                    <Text>Track List</Text>
+                </Dialog.Title>
                 <Dialog.ScrollArea>
                     <RecyclerListView
                         style={{ height: SCREEN_HEIGHT - 150 }}
@@ -37,8 +48,42 @@ export default function TrackListModal({ trackList, visible, hideDialog })
     );
 }
 
+export default React.memo(TrackListModal);
+
 const styles = StyleSheet.create({
     trackListDialog: {
-        height: 500
+        height: 500,
+        borderRadius: 15
+    },
+    // Card
+    songCard: {
+        borderWidth: 1,
+        flexDirection: "row",
+        borderColor: "#dedede",
+        margin: 5,
+        padding: 10,
+        borderRadius: 5,
+    },
+
+    imgBox: {
+        flex: 1
+    },
+    avatarIMG: {
+        width: 50,
+        height: 50,
+        borderRadius: 5
+    },
+
+    inforBox: {
+        flex: 4,
+        marginLeft: 15,
+        alignSelf: "center"
+    },
+    songTitle: {
+        fontSize: 18
+    },
+    songCreator: {
+        fontSize: 12,
+        color: "#959596"
     }
 });
