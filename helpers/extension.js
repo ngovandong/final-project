ea69d5a10f7bfb93c87a3882f479de3dcd27a133
phuocleoceo <ht10082001@gmail.com>
2022-06-18 13:39:07 +0700

    Fix TrackList Re-render

--- helpers/extension.js
+++ helpers/extension.js
@@ -33,11 +33,3 @@ export function getSongDuration(duration)
     }
     return "0:00";
 }
-
-export function getAudioTimeString(seconds)
-{
-    const h = parseInt(seconds / (60 * 60));
-    const m = parseInt(seconds % (60 * 60) / 60);
-    const s = parseInt(seconds % 60);
-    return ((m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s));
-}
